f2cb600138fcb0575c579b62795a936a6762a1bd
Colin Armstrong <colinwarmstrong@gmail.com>
2019-02-03 22:37:50 -0700

    Add ability to parse JSON files to ReservationSystem class

--- lib/reservation_system.rb
+++ lib/reservation_system.rb
@@ -1,2 +1,20 @@
+require 'json'
+require 'pry'
+
 class ReservationSystem
+  attr_reader :data
+
+  def initialize(json_file_path)
+    @data = parse_json(json_file_path)
+  end
+
+  def parse_json(json_file_path)
+    JSON.parse(read_file(json_file_path), symbolize_names: true)
+  end
+
+  private
+
+  def read_file(file_path)
+    File.read(file_path)
+  end
 end
--- test/reservation_system_test.rb
+++ test/reservation_system_test.rb
@@ -2,4 +2,22 @@ require_relative 'test_helper.rb'
 require_relative '../lib/reservation_system.rb'
 
 class ReservationSystemTest < Minitest::Test
+  def test_reservation_system_exists
+    file_path = './test-case.json'
+    reservation_system = ReservationSystem.new(file_path)
+
+    assert_instance_of ReservationSystem, reservation_system
+  end
+
+  def test_reservation_system_can_parse_json_files
+    file_path = './test-case.json'
+    reservation_system = ReservationSystem.new(file_path)
+
+    data = reservation_system.data
+
+    assert_instance_of Hash, data
+    assert_instance_of Hash, data[:search]
+    assert_instance_of Array, data[:campsites]
+    assert_instance_of Array, data[:reservations]
+  end
 end
